77d693a48dcfe079c27e9e2bc3323d60a3231a47
pedernin <pedernin@students.zhaw.ch>
2022-02-09 19:46:50 +0100

    changed margins

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,7 +1,7 @@
 <template>  
   <div>
     <h1>REGISTER</h1>
-      <div class="flex justify-center items-center mt-52">
+      <div class="flex justify-center items-center mt-20">
         <div class="w-full max-w-xs">
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" @submit.prevent="register">
 
