35ba68daa5259996df8faea596d48ac6999665de
pedernin <pedernin@students.zhaw.ch>
2022-02-18 18:04:49 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router"
 import store from './store'
 import App from './App.vue'
 
-const app = createApp(App).
+const app = createApp(App);
 app.use(store);
 app.use(router);
 app.mount('#app');
