f96252c5b965d5859f45db6ac3165a317c6fca74
ninopedergnana <pedergnana.nino@gmail.com>
2022-09-22 21:31:37 +0200

    test

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="bg-red-400">
+    <div class="mx-auto flex-col max-w-3xl py-12 px-4 sm:flex sm:items-start sm:px-6 lg:flex lg:items-start 
+		lg:justify-between lg:py-16 lg:px-8">
+
+    <!--Project Name-->
+    <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        <span class="block text-indigo-600">{{ projectName }}</span>
+      </h2>
+
+    <!--Project Description-->
+    <h2 class="text-2xl font-bold flex justify-items-start tracking-tight text-gray-900 sm:text-2xl">
+        <span class="block">{{ projectDescription }}</span>
+      </h2>
+
+
+
+        <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+				<!--button 1-->
+        <div class="inline-flex rounded-md shadow">
+          <a href="#" class="inline-flex items-center justify-center 
+					rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base 
+					font-medium text-white hover:bg-indigo-700">Get started</a>
+        </div>
+
+
+				<!--button 2-->
+        <div class="ml-3 inline-flex rounded-md shadow">
+          <a href="#" class="inline-flex items-center justify-center rounded-md 
+					border border-transparent bg-white px-5 py-3 text-base font-medium 
+					text-indigo-600 hover:bg-indigo-50">Learn more</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+  
+  <script setup lang=ts>
+	import { ref } from 'vue'
+
+	defineProps<{
+		projectName?: string
+		projectDescription?: string
+	}>()
+
+
+
+  </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
 <template>
 <div>
     <h1>HOME</h1>
-    <p>CV</p>
+    <ProjectCard 
+        v-for="(project, index) in projects" 
+        :project-name="projects[index].projectName" 
+        :project-description="projects[index].projectDescription"
+    />
 </div>
 
 </template>
 
-<script>
-import Clicker from "../components/Clicker.vue"
+<script setup>
+import ProjectCard from "../components/ProjectCard.vue"
 
-export default {
-    components: { Clicker }
-}
+const projects = [
+    { projectName: 'Project 1', projectDescription: 'It dictates what amount of the available space inside the flex container the item should take up. If all items have flex-grow set to 1, the remaining space in the container will be distributed equally to all children. If one of the children has a value of 2, that child would take up twice as much of the space either one of the others (or it will try, at least).'},
+    { projectName: 'Project 2', projectDescription: 'Description 2'},
+    { projectName: 'Project 3', projectDescription: 'Description 3'}
+]
 </script>
 
 <style>
